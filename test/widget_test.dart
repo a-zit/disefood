c014f964d8675f79d601dfd427741869a5d6d30c
nawapan2541 <nawapan_deeprasertkul@hotmail.com>
2020-03-30 22:43:43 +0700

    Commit by Tae : home_seller,home_customer,sidemenu_customer,sidemenu_seller

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -4,7 +4,8 @@
 // utility that Flutter provides. For example, you can send tap and scroll
 // gestures. You can also use WidgetTester to find child widgets in the widget
 // tree, read text, and verify that the values of widget properties are correct.
-
+import 'package:disefood/screen/home_seller.dart';
+import 'package:disefood/screen/home_customer.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
@@ -13,7 +14,7 @@ import 'package:disefood/main.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(MyApp());
+    await tester.pumpWidget(Home());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
