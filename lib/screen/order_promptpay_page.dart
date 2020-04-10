736588de2326b7698f168ab2f8f276fb8a3ad961
natthapasboom <natthapas.boom@mail.kmutt.ac.th>
2020-04-10 22:58:57 +0700

    Commited By Boom : [UPDATE] order_promptpay

--- lib/screen/order_promptpay_page.dart
+++ lib/screen/order_promptpay_page.dart
@@ -7,6 +7,7 @@ class PromptpayPage extends StatefulWidget {
 }
 
 class _PromptpayPageState extends State<PromptpayPage> {
+  bool noupload = true;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -22,60 +23,191 @@ class _PromptpayPageState extends State<PromptpayPage> {
         ],
       ),
       drawer: SideMenuCustomer(),
-      body: Container(
-        color: Colors.grey[200],
-        child: Column(
-          children: <Widget>[
-            Container(
-              height: 400,
-              margin: EdgeInsets.fromLTRB(0, 10, 0, 10),
-              padding: EdgeInsets.fromLTRB(40, 30, 40, 0),
-              child: Card(
-                shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(8)),
-                child: Column(
-                  children: <Widget>[
-                    Container(
-                      width: 150,
-                      margin: EdgeInsets.only(top: 30),
-                      child: Image.network(
-                          "https://www.thaiload.com/wp-content/uploads/2018/01/image.jpeg"),
-                    ),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: <Widget>[
-                        Text("ร้าน 000001"),
-                        Text(" # 999999 Baht"),
-                      ],
-                    ),
-                    Container(
-                      width: 200,
-                      margin: EdgeInsets.only(top: 10),
-                      child: Image.network(
-                          "https://boofcv.org/images/thumb/3/35/Example_rendered_qrcode.png/400px-Example_rendered_qrcode.png"),
-                    ),
-                  ],
+      body: SingleChildScrollView(
+        child: Container(
+          color: Colors.grey[200],
+          child: Column(
+            children: <Widget>[
+              Container(
+                alignment: Alignment.centerLeft,
+                margin: EdgeInsets.fromLTRB(45, 30, 0, 0),
+                child: Text(
+                  "การชำระเงิน",
+                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 18),
                 ),
               ),
-            ),
-            Container(
-              width: 200,
-              child: FlatButton(
-                onPressed: () {},
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: <Widget>[
-                    Icon(Icons.file_download,color: Colors.orange,),
-                    Text(" Download QRcode",style: TextStyle(color: Colors.orange,fontWeight: FontWeight.bold),),
-                  ],
-                ),
-                shape: RoundedRectangleBorder(
-                  side: BorderSide(color: Colors.orange,width: 2),
-                  borderRadius: BorderRadius.circular(20),
+              Divider(
+                thickness: 3,
+                indent: 45,
+                endIndent: 45,
+              ),
+              Container(
+                height: 400,
+                padding: EdgeInsets.fromLTRB(40, 0, 40, 0),
+                child: Card(
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(8)),
+                  child: Column(
+                    children: <Widget>[
+                      Container(
+                        width: 150,
+                        margin: EdgeInsets.only(top: 30),
+                        child: Image.network(
+                            "https://www.thaiload.com/wp-content/uploads/2018/01/image.jpeg"),
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: <Widget>[
+                          Text("ร้าน 000001"),
+                          Text(" # 999999 Baht"),
+                        ],
+                      ),
+                      Container(
+                        width: 200,
+                        margin: EdgeInsets.only(top: 10),
+                        child: Image.network(
+                            "https://boofcv.org/images/thumb/3/35/Example_rendered_qrcode.png/400px-Example_rendered_qrcode.png"),
+                      ),
+                      Container(
+                        margin: EdgeInsets.only(top: 15),
+                        width: 200,
+                        child: FlatButton(
+                          onPressed: () {},
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: <Widget>[
+                              Icon(
+                                Icons.file_download,
+                                color: Colors.orange,
+                              ),
+                              Text(
+                                " บันทืกรูปภาพ",
+                                style: TextStyle(
+                                    color: Colors.orange,
+                                    fontWeight: FontWeight.bold),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
                 ),
               ),
-            )
-          ],
+              Container(
+                alignment: Alignment.centerLeft,
+                margin: EdgeInsets.fromLTRB(45, 20, 0, 0),
+                child: Text(
+                  "อัพโหลดสลิปการโอน",
+                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 18),
+                ),
+              ),
+              Divider(
+                thickness: 3,
+                indent: 45,
+                endIndent: 45,
+              ),
+              Container(
+                width: 330,
+                child: Card(
+                  child: Column(
+                    children: <Widget>[
+                      Visibility(
+                        visible: true,
+                        child: Container(
+                          width: 300,
+                          child: Image.network(
+                              "https://wakarusaag.com/wp-content/plugins/oem-showcase-inventory/assets/images/noimage-found.png"),
+                        ),
+                        replacement: SizedBox(
+                          height: 480,
+                          child: Container(
+                            margin: EdgeInsets.fromLTRB(10, 30, 10, 10),
+                            height: 360,
+                            width: 300,
+                            child: Container(
+                              height: 500,
+                              margin: EdgeInsets.only(top: 10),
+                              child: Image.network(
+                                  "https://www.kasikornbank.com/SiteCollectionDocuments/personal/digital-banking/kplus/functions/verified-slip/img/img-03.png"),
+                            ),
+                          ),
+                        ),
+                      ),
+                      Container(
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: <Widget>[
+                            FlatButton(
+                              color: Colors.white,
+                              child: Row(
+                                children: <Widget>[
+                                  Icon(
+                                    Icons.file_upload,
+                                    color: Colors.orange,
+                                  ),
+                                  Text(
+                                    "เลือกรูปภาพ",
+                                    style: TextStyle(
+                                        fontWeight: FontWeight.bold,
+                                        color: Colors.orange),
+                                  )
+                                ],
+                              ),
+                              onPressed: () {},
+                            ),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Container(
+                    width: 150,
+                    margin: EdgeInsets.fromLTRB(0, 10, 20, 20),
+                    child: RaisedButton(
+                      color: Colors.white,
+                      onPressed: () {},
+                      child: Text(
+                        "ย้อนกลับ",
+                        style: TextStyle(
+                            color: Colors.orange,
+                            fontWeight: FontWeight.bold,
+                            fontSize: 18),
+                      ),
+                      shape: RoundedRectangleBorder(
+                        side: BorderSide(color: Colors.white, width: 2),
+                        borderRadius: BorderRadius.circular(5),
+                      ),
+                    ),
+                  ),
+                  Container(
+                    width: 150,
+                    margin: EdgeInsets.fromLTRB(0, 10, 0, 20),
+                    child: RaisedButton(
+                      color: Colors.orange,
+                      onPressed: () {},
+                      child: Text(
+                        "ยืนยัน",
+                        style: TextStyle(
+                            color: Colors.white,
+                            fontWeight: FontWeight.bold,
+                            fontSize: 18),
+                      ),
+                      shape: RoundedRectangleBorder(
+                        side: BorderSide(color: Colors.orange, width: 2),
+                        borderRadius: BorderRadius.circular(5),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
         ),
       ),
     );
